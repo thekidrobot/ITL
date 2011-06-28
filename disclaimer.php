7a65f7982dd22d5cde5b9960f8b5c84671bccd1f
Andres Gonzalez <andgonzalez81@gmail.com>
2011-06-28 11:04:08 +0530

    Disclaimer Page

---
+++ disclaimer.php
@@ -0,0 +1,50 @@
+<?php include("includes/main/head.php"); ?>
+<body>	
+<!--container-->
+<div class="container">
+	<!--header-->
+	<?php include("includes/main/top_banner.php"); ?>
+	<!--end_header-->
+	<!--content-->
+	<div class="content">
+		<!-- main content -->
+		<div class="core">
+			<div class="core_top"><img src="images/content_top.jpg" alt="" /></div>
+			<div class="core_content">
+				<div class="welcome_txt">
+					<h1>Disclaimer</h1>
+          <p>The information and materials on this website was prepared by
+          Intercontinental Trust Limited to provide potential clients with a
+          broad overview of the opportunities available in Mauritius. While all
+          reasonable care has been taken in the preparation of this website,
+          Intercontinental Trust Limited accepts no responsibility for any
+          errors it may contain, whether caused by negligence or otherwise, or
+          for any loss, however caused, sustained by any person that relies on
+          it. Readers are advised to consult with appropriate, qualified
+          professional advisors before taking action. Intercontinental Trust
+          Limited will be pleased to discuss any specific issues.<br /><br />
+          This website contains links which may lead to other websites.
+          Intercontinental Trust Limited does not necessarily sponsor, endorse
+          or otherwise provide any warranty as to the accuracy or completeness
+          of the contents of such linked websites.
+				</p>
+				</div>
+				<div class="cleaner"></div>
+				<br />
+			</div>
+			<div class="core_btm"><img src="images/content_btm.jpg" alt="" /></div>
+		</div>
+		<!-- end main content -->
+		<!-- side content-->
+		<?php include("includes/main/side_content.php"); ?>		
+		<!-- end side content-->
+		<div class="cleaner"></div>
+	</div>
+	<!--end_content-->
+	<!--footer-->
+	<?php include("includes/main/footer.php"); ?>
+	<!--end_footer-->
+</div>
+<!--end_container-->			
+</body>
+</html>
