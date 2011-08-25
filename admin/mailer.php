944fe8d31c3f064a353e1b6bb5624739df3d940c
Andres Gonzalez <andgonzalez81@gmail.com>
2011-08-25 17:26:42 +0530

    Mailer to test

--- admin/mailer.php
+++ admin/mailer.php
@@ -4,11 +4,14 @@ include('../functions/ps_pagination.php');
 
 if(isset($_POST['Process']))
 {
-  $errors = "";
+  $msg = "";
   $allowed_extensions = array("csv");
   $max_allowed_file_size = 2048; // size in KB
   $documents_path = "documents/mailer";
 			  
+  $subject = $_POST['subject'];
+  $content = $_POST['content'];
+  
   //File validation
 	
   //Get the name
@@ -16,7 +19,17 @@ if(isset($_POST['Process']))
   
   if(trim($filename) == "")
   {
-	$msg .= "\n Please upload a CSV file.\n <br />";
+    $msg .= "\n Please upload a CSV file.\n <br />";
+  }
+  
+  if(trim($subject) == "")
+  {
+    $msg .= "\n Please fill in mail subject.\n <br />";
+  }
+  
+  if(trim($content) == "")
+  {
+    $msg .= "\n Please fill in mail content.\n <br />";
   }
   
   //get the extension
@@ -35,71 +48,72 @@ if(isset($_POST['Process']))
   $allowed_ext = false;
   for($i=0; $i<sizeof($allowed_extensions); $i++)
   {
-	if(strcasecmp($allowed_extensions[$i],$filetype) == 0)
-	{
-	  $allowed_ext = true;
-	}
+    if(strcasecmp($allowed_extensions[$i],$filetype) == 0)
+    {
+      $allowed_ext = true;
+    }
   }
    
   if(!$allowed_ext)
   {
-	$msg.= "\n The uploaded filetype not a supported file type.\n <br />".
-		   " Only the following file types are supported: ".implode(',',$allowed_extensions)."\n <br />";
+    $msg.=  "\n The uploaded filetype not a supported file type.\n <br />".
+            " Only the following file types are supported: ".implode(',',$allowed_extensions)."\n <br />";
   }
   
   if(trim($msg) == '')
   {
-	if ($_FILES["file"]["error"] > 0)
-	{
-	  $msg="Return Code: " . $_FILES["file"]["error"] . "<br />";
-	}
-	else
-	{
-	  if(!is_dir($documents_path)) mkdir($documents_path,0777);
-	  
-	  $filepath = $documents_path.'/'."mailer.csv";
-	  
-	  if (file_exists($filepath))
-	  {
-		unlink($filepath);
-	  }
-	  else
-	  {
-		$tmp_path = $_FILES["file"]["tmp_name"];
-		if(is_uploaded_file($tmp_path))
+		if ($_FILES["file"]["error"] > 0)
 		{
-		  if(!move_uploaded_file($tmp_path,$filepath))
-		  {
-			$msg.= "Error uploading file: " . $_FILES["file"]["error"] . "<br />";
-		  }
+			$msg="Return Code: " . $_FILES["file"]["error"] . "<br />";
 		}
 		else
 		{
-		  $max_line_length=10000;
-		  if (($handle = fopen("mailer.csv", "r")) !== FALSE)
-		  {
-			$columns = fgetcsv($handle, $max_line_length, ",");
-			foreach ($columns as &$column)
+			if(!is_dir($documents_path)) mkdir($documents_path,0777);
+			
+			$filepath = $documents_path.'/'."mailer.csv";
+			
+			if (file_exists($filepath))
 			{
-				$column = strtolower(str_replace(".","",$column));
+				unlink($filepath);
 			}
 			
-			mysql_query("delete from mailer");
-			
-			$insert_query_prefix = "INSERT IGNORE INTO mailer (".join(",",$columns).")\nVALUES";
-			
-			while (($data = fgetcsv($handle, $max_line_length, ",")) !== FALSE)
+			$tmp_path = $_FILES["file"]["tmp_name"];
+			if(!move_uploaded_file($tmp_path,$filepath))
 			{
-				while (count($data)<count($columns)) array_push($data, NULL);
-				$query = "$insert_query_prefix ('".join("','",$data)."');";
-				mysql_query($query);
+				$msg.= "Error uploading file: " . $_FILES["file"]["error"] . "<br />";
 			}
-			fclose($handle);
-		  }
-		  
-		}
-	  }
-	}   
+			else
+			{
+				$path_only = implode("/", (explode('/', $_SERVER["SCRIPT_FILENAME"], -1)));
+				
+				if (($handle = fopen($filepath, "r")) !== FALSE)
+				{
+					$columns = fgetcsv($handle, $max_line_length, ",");
+					foreach ($columns as &$column)
+					{
+						$column = strtolower(str_replace(".","",$column));
+					}
+				
+					mysql_query("delete from mailer");
+					
+					$insert_query_prefix = "INSERT IGNORE INTO mailer (".join(",",$columns).")\nVALUES";
+				
+					while (($data = fgetcsv($handle, $max_line_length, ",")) !== FALSE)
+					{
+						while (count($data)<count($columns)) array_push($data, NULL);
+						$query = "$insert_query_prefix ('".join("','",$data)."');";
+						mysql_query($query);
+						
+						//Send Mail
+						sendemail(escape_value($data[2]),escape_value($subject),escape_value($content));
+						
+            $msg = "File processed sucessfully";
+            
+					}
+					fclose($handle);
+				}
+			}
+		}   
   }  
 }
   
@@ -111,40 +125,44 @@ if(isset($_POST['Process']))
   <?php include("includes/menu.php"); ?>
 	<div id="containerHolder">
 	  <div id="container">
-	    <div id="sidebar"><?php include('includes/side_menu.php'); ?></div>    
+      <div id="sidebar"><?php include('includes/side_menu.php'); ?></div>    
 	    <div id="main">
 	      <h2>Mass mail process</h2>
-		  <h4><a href="documents/mailer/template.csv">Download the template</a></h4>
-          <form method="post" name="doc_form" enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>">
-          <?
-			if($msg!="") echo "<br /><font color='red'>$msg</font>";
-          ?>
-			<div id="errorDate2"></div>
-            <table cellpadding="0" cellspacing="0">
-            <?php
-              ?>	
-              <tr>
-				<td><b>Document Upload: </b></td>
-                <td><input type="file" name="file" id="file" readonly /></td>
-              </tr> 
-              <tr>
-				<td>
-				  &nbsp;
-				</td>
-				<td>
-				  <p><input type="submit" class="button-submit" value="Process" name="Process" id ="add" /></p>
-			  	</td>
-			  </tr>
-            </table>
-          </form>
+        <h4><a href="documents/mailer/template.csv">Download the template</a></h4>
+        <form method="post" name="doc_form" enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>">
+        <?php
+          if($msg!="") echo "<br /><font color='red'>$msg</font>";
+        ?>
+        <div id="errorDate2"></div>
+          <table cellpadding="0" cellspacing="0">
+            <tr>
+              <td><b>Subject: </b></td>
+              <td><input type="text" name="subject" value="<?=$_POST['subject']?>" /></td>
+            </tr>
+  		  	  <tr>
+              <td><b>Content: </b></td>
+              <td><textarea name="content" rows="5" cols="20"><?=$_POST['content']?></textarea></td>
+            </tr>
+            <tr>
+              <td><b>Recipients: </b></td>
+              <td><input type="file" name="file" id="file" readonly /></td>
+            </tr> 
+            <tr>
+              <td>&nbsp;</td>
+              <td>
+                <p><input type="submit" class="button-submit" value="Process" name="Process" id ="add" /></p>
+              </td>
+            </tr>
+          </table>
+        </form>
         </div>
         <!-- // #main -->
-	  <div class="clear"></div>
-    </div>
+        <div class="clear"></div>
+      </div>
     <!-- // #container -->
-  </div>	
-  <!-- // #containerHolder -->       
-	<?php include('includes/footer.php');?> 
+    </div>	
+    <!-- // #containerHolder -->       
+    <?php include('includes/footer.php');?> 
   </div>
   <!-- // #wrapper -->
 </body>
